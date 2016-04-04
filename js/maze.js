046cd7b526e92bfcee6665d44c9d8756012bc961
Heya Na <naheya1126@gmail.com>
2016-04-04 18:02:40 -0400

    Create 2d array mazespace. Each mazespace has 4 directions.

--- js/maze.js
+++ js/maze.js
@@ -12,6 +12,15 @@ function Maze(width, height) {
     this.startOrientation = null;
     this.endX = null;
     this.endY = null;
+    this.spaces = [];
+
+    var x, y;
+    for(x = 1; x <= width; x++){
+        this.spaces[x] = [];
+        for(y = 1; y<= height; y++){
+            this.spaces[x][y] = new MazeSpace();
+        }
+    }
 }
 
 Maze.prototype.setStart = function (x, y, orientation) {
@@ -23,4 +32,8 @@ Maze.prototype.setStart = function (x, y, orientation) {
 Maze.prototype.setEnd = function (x, y) {
     this.endX = x;
     this.endY = y;
+}
+
+Maze.prototype.setWall = function (x, y, direction) {
+    this.spaces[x][y].setWall(direction);
 }
--- js/mazespace.js
+++ js/mazespace.js
@@ -1,3 +1,15 @@
 /**
  * Created by Heya on 2016-04-03.
  */
+"use strict"
+
+function  MazeSpace() {
+    this.north = false;
+    this.east = false;
+    this.south = false;
+    this.west = false;
+}
+
+MazeSpace.prototype.setWall = function (direction) {
+    this[direction] = true;
+}
