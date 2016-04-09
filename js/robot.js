c8fb7f36d03c239644502bafa4cf9db5b748c2a9
Heya Na <naheya1126@gmail.com>
2016-04-08 21:06:27 -0400

    Modified move forward validation for maze and robot

--- js/robot.js
+++ js/robot.js
@@ -45,6 +45,11 @@ Robot.prototype.turnLeft = function(){
 }
 
 Robot.prototype.moveForward = function () {
+
+    if(!this.canMoveForward()){
+        return false;
+    }
+
     switch(this.orientation){
         case "north":
             this.y += 1;
@@ -53,10 +58,20 @@ Robot.prototype.moveForward = function () {
             this.x += 1;
             break;
         case "south":
-            this.y += 1;
+            this.y -= 1;
             break;
         case "west":
-            this.x += 1;
+            this.x -= 1;
             break;
     }
+
+    return true;
+}
+
+Robot.prototype.canMoveForward = function () {
+    if(!this.maze){
+        return false;
+    }
+    return this.maze.canMove(this.x, this.y, this.orientation);
+
 }
